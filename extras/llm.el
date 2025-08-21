ae35165bdaabffed82fb8a428ba80bd45a5723e4
Shuo Chen <shuo.chen@advance.ai>
2025-08-21 20:04:20 +0800

    two llm model config

--- extras/llm.el
+++ extras/llm.el
@@ -2,9 +2,23 @@
   :ensure t
   :bind ("C-c e" . gptel)
   :config
+  (defun get-ollama-models ()
+  "Fetch the list of installed Ollama models."
+  (let* ((output (shell-command-to-string "ollama list"))
+         (lines (split-string output "\n" t))
+         models)
+    (dolist (line (cdr lines))  ; Skip the first line
+      (when (string-match "^\\([^[:space:]]+\\)" line)
+        (push (match-string 1 line) models)))
+    (nreverse models)))
+
+  (gptel-make-ollama "rOllama"
+                 :host "192.168.1.28:11434"
+                 :stream t
+                 :models (get-ollama-models))
   (setq
    gptel-model 'gemma3:latest
    gptel-backend (gptel-make-ollama "Ollama"
                  :host "localhost:11434"
                  :stream t
-                 :models '(gemma3:latest))))
+                 :models (get-ollama-models))))
